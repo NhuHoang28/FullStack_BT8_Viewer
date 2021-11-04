8059afea28c6ec737e48d5647168f387286ec35c
ThuNhu2801 <hoangthithunhu28012001@gmail.com>
2021-11-04 19:40:32 +0700

    modal

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Viewer
 class viewListener {
     constructor(id) {
         this.id = id;
@@ -26,3 +27,40 @@ class viewListener {
     }
 }
 new viewListener('viewer1');
+// modal
+class modal {
+    constructor(id) {
+        this.id = id;
+        this.getListeners();
+        this.getElement();
+        this.setEvent();
+        this.setAttribute();
+    }
+    setAttribute() {
+        this.element.style.display = 'none';
+    }
+    getListeners() {
+        this.listeners = document.querySelectorAll('*[modal-open=true][modal-id=' + this.id + ']');
+    }
+    getElement() {
+        this.element = document.getElementById(this.id);
+        this.elementMask = document.querySelector('#' + this.id + ' .modal-mask');
+    }
+    setToggle() {
+        if (this.element.style.display == 'block')
+            this.element.style.display = 'none';
+        else this.element.style.display = 'block';
+    }
+
+    setEvent() {
+        this.listeners.forEach(listener => {
+            listener.onclick = e => {
+                this.setToggle();
+            }
+        })
+        this.elementMask.onclick = e => {
+            this.setToggle();
+        }
+    }
+}
+new modal('modal1');
